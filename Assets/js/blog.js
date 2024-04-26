a82e61c9308909c6772adb67cfa416ae8bfb73ac
Bung <android.sitko@gmail.com>
2024-04-26 04:33:45 -0700

    Core Javascript in place, everything finished

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -1,20 +1,54 @@
+//Parses data onto new page, gathers index number by subtracting one from length
+
 function renderBlogData() {
     const myArray = JSON.parse(localStorage.getItem('myArray'));
 
     let lastIndex = (myArray.length - 1);
 
-    console.log(lastIndex);
+//Basic for loop counter, counts down for each object in array. Will put more recent posts above.
 
    for (lastIndex; lastIndex > -1; lastIndex--) {
     const cookedblog = myArray[lastIndex];
 
+//Extra security for the app to not break, null objects aren't allowed anyway but double up anyways...
+
     if (cookedblog !== null) {
-        document.getElementById('saved-user').innerHTML = cookedblog.username;
-        document.getElementById('saved-title').innerHTML = cookedblog.title;
-        document.getElementById('saved-blog').innerHTML = cookedblog.blog;
-    }
+
+//Create a new div for each object from previous loop, give it a unique id that counts up
+
+        const newDiv = document.createElement('div');
+        newDiv.setAttribute('id',`post-${lastIndex}`);
+        newDiv.setAttribute('class','posts');
+        document.body.appendChild(newDiv);
+
+//Nested for loop, another basic counter this time implementing i. goes from 0-2 and places the object's children where they need to go with the styles they need.
+
+        for(i = 0; i < 3; i++) {
+            const choppedblog = i
+
+            if (choppedblog == 0) {
+               const user = document.createElement('h4');
+               user.textContent = cookedblog.username;
+               document.body.appendChild(user);
+            }
+
+            else if (choppedblog == 1) {
+                const postTitle = document.createElement('h3');
+                postTitle.textContent = cookedblog.title;
+                document.body.appendChild(postTitle);
+            }
+
+            else {
+                const blogtent = document.createElement('p');
+                blogtent.textContent = cookedblog.blog;
+                document.body.appendChild(blogtent);
+            }
+        }
     }
 }
+}
+
+//Basic initial function that makes things happen when the page loads.
 
 function init() {
     renderBlogData();
--- Assets/js/form.js
+++ Assets/js/form.js
@@ -2,6 +2,7 @@ const username = document.getElementById('user-msg');
 const title = document.getElementById('title-msg');
 const blog = document.getElementById('blog-msg');
 const submitButton = document.getElementById('submit');
+const jlButton = document.getElementById('just-lookin');
 
 //Create object for all information to get saved into localstorage
 
@@ -11,6 +12,8 @@ function saveBlogData() {
         title: title.value.trim(),
         blog: blog.value.trim(),
     };
+
+    //Make sure that the blog is fully filled out before sending it
 
     if (!rawblog.username) {
         alert('Username required!')
@@ -26,17 +29,37 @@ function saveBlogData() {
         alert("You can't write nothing!")
         return;
     }
+
+    // If it passes parse the local storage for previous posts, if there are none create an index, if not add to the index
+
     else {
-    const myArray = [];
+        const myArray = JSON.parse(localStorage.getItem('myArray'));
+        console.log(myArray);
+        if (myArray == null) {
+            const myArray = [];
+            myArray.push(rawblog);
+            localStorage.setItem('myArray', JSON.stringify(myArray));
+            window.location.href="./blog.html"
+        }
 
-    myArray.push(rawblog);
+        else {
+        myArray.push(rawblog);
 
-    localStorage.setItem('myArray', JSON.stringify(myArray));
+        localStorage.setItem('myArray', JSON.stringify(myArray));
+        window.location.href="./blog.html"
+        }
 }
 }
+//Listen for click on submit button, then execute code.
 
 submitButton.addEventListener('click', function (event) {
     event.preventDefault();
     saveBlogData();
+});
+
+//Button if you want to see the feed but not add any posts
+
+jlButton.addEventListener('click', function (event) {
+    event.preventDefault();
     window.location.href="./blog.html"
 });
